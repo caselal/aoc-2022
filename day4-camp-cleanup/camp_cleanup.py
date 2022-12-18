7d04e4a1de5542c5f8757eac3e07670bb564bb82
Cassandra Luong <cassandraluong@gmail.com>
2022-12-17 23:34:46 -0800

    Create function to use in AOC Day 4, Part One and Two

--- day4-camp-cleanup/camp_cleanup.py
+++ day4-camp-cleanup/camp_cleanup.py
@@ -2,10 +2,25 @@
 Advent of Code - Day 4: Camp Cleanup
 """
 
+# load input file
 with open('input.txt', 'r') as input:
     sap = input.read()
 
 sap = sap.splitlines()
+
+
+# Function to split each line of input into pairs to compare section assignments for each pair
+def sub_pair(pair):
+    elf_one_start = pair.split(',')[0].split('-')[0]
+    elf_one_end = pair.split(',')[0].split('-')[1]
+    elf_two_start = pair.split(',')[1].split('-')[0]
+    elf_two_end = pair.split(',')[1].split('-')[1]
+
+    elf_one = set(list(range(int(elf_one_start), int(elf_one_end) + 1)))
+    elf_two = set(list(range(int(elf_two_start), int(elf_two_end) + 1)))
+
+    return elf_one, elf_two
+
 
 ############
 # Part One
@@ -14,17 +29,7 @@ sap = sap.splitlines()
 count_assignment_pairs = 0
 for pair in sap:
 
-    sub_pair_one_start = pair.split(',')[0].split('-')[0]
-    sub_pair_one_end = pair.split(',')[0].split('-')[1]
-    sub_pair_two_start = pair.split(',')[1].split('-')[0]
-    sub_pair_two_end = pair.split(',')[1].split('-')[1]
-
-    sub_pair_one = set(
-        list(range(int(sub_pair_one_start),
-                   int(sub_pair_one_end) + 1)))
-    sub_pair_two = set(
-        list(range(int(sub_pair_two_start),
-                   int(sub_pair_two_end) + 1)))
+    sub_pair_one, sub_pair_two = sub_pair(pair)
 
     if sub_pair_one.issubset(sub_pair_two) or sub_pair_two.issubset(
             sub_pair_one):
@@ -38,17 +43,7 @@ print(count_assignment_pairs)
 count_assignment_pairs_2 = 0
 for pair in sap:
 
-    sub_pair_one_start = pair.split(',')[0].split('-')[0]
-    sub_pair_one_end = pair.split(',')[0].split('-')[1]
-    sub_pair_two_start = pair.split(',')[1].split('-')[0]
-    sub_pair_two_end = pair.split(',')[1].split('-')[1]
-
-    sub_pair_one = set(
-        list(range(int(sub_pair_one_start),
-                   int(sub_pair_one_end) + 1)))
-    sub_pair_two = set(
-        list(range(int(sub_pair_two_start),
-                   int(sub_pair_two_end) + 1)))
+    sub_pair_one, sub_pair_two = sub_pair(pair)
 
     if len((sub_pair_one & sub_pair_two)) > 0:
         count_assignment_pairs_2 += 1
